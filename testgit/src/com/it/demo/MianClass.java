9bb4e3b4f6734e6e3d69df539f3b1ce8bca4d16b
541661382 <541661382@qq.com>
2020-09-25 11:31:05 +0800

    修改sdfsd

--- testgit/src/com/it/demo/MianClass.java
+++ testgit/src/com/it/demo/MianClass.java
@@ -3,6 +3,7 @@ package com.it.demo;
 public class MianClass {
     public static void main(String[] args) {
         int a = 0;
+        String dfhgdsfg;
         System.out.println("rwefd");
     }
 }
