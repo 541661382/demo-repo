8957eaaa36fbcd5a5567a195818e1283c7d9239e
541661382 <541661382@qq.com>
2020-09-25 11:29:35 +0800

    删除文件

--- testgit/src/com/it/demo/MianClass.java
+++ testgit/src/com/it/demo/MianClass.java
@@ -3,6 +3,7 @@ package com.it.demo;
 public class MianClass {
     public static void main(String[] args) {
         int a = 0;
+        String asdfsadf;
         System.out.println("rwefd");
     }
 }
